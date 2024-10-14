9dfa0d230e0e56b5fd026d6035411c8f01460e92
steveostudios <steveostudios@gmail.com>
2024-10-14 11:56:56 -0400

    setup db

--- src/data/config.ts
+++ src/data/config.ts
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAzqZc0XrJpT02CHOhfGYsEPsi7TPIA0vs",
+  authDomain: "around-the-horn-6cf7b.firebaseapp.com",
+  projectId: "around-the-horn-6cf7b",
+  storageBucket: "around-the-horn-6cf7b.appspot.com",
+  messagingSenderId: "914238648437",
+  appId: "1:914238648437:web:ff2175da9601c74ac8483c",
+  measurementId: "G-W0J345ZC62",
 };
 
 const app = initializeApp(firebaseConfig);
